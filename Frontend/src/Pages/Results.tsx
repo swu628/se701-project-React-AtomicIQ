eeb2328a537f198ffc893a45c53f98a1bbe3f57d
kper068 <kper068@aucklanduni.ac.nz>
2024-05-25 21:53:17 +1200

    Feat: Basic page layout

--- Frontend/src/Pages/Results.tsx
+++ Frontend/src/Pages/Results.tsx
@@ -1,15 +1,22 @@
-import { Box, Paper, Typography } from "@mui/material";
+import { Box, Paper, Stack, Typography } from "@mui/material";
+import { useEffect } from "react";
 
 export default function Results() {
+  useEffect(() => {
+    document.body.classList.add("backgroundImage");
+    return () => {
+      document.body.classList.remove("backgroundImage");
+    };
+  }, []);
+
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      mt="7.5vh"
-      sx={{ verticalAlign: "middle" }}
-    >
+    <Box display="flex" mt="7.5vh" justifyContent="center">
       <Paper elevation={3} sx={{ width: "80vw", minHeight: "75vh", p: "2rem" }}>
-        <Typography>Results</Typography>
+        <Stack display="flex" direction="column">
+          <Typography variant="h5" fontWeight="bold" alignSelf="center">
+            Quiz Results
+          </Typography>
+        </Stack>
       </Paper>
     </Box>
   );
