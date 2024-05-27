b5b883f1be02baa067599e922ad8d0cca1ad5da3
jgai284 <jgai284@aucklanduni.ac.nz>
2024-05-27 14:17:33 +1200

    completed home page

--- Frontend/src/Pages/Home.tsx
+++ Frontend/src/Pages/Home.tsx
@@ -1,4 +1,6 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import { Container, Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   useEffect(() => {
@@ -8,9 +10,53 @@ export default function Home() {
     };
   }, []);
 
+  const containerStyles = {
+    p: "2rem",
+    minHeight: "92.75vh",
+    width: "80vw",
+    boxShadow: 3,
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "white",
+    alignItems: "center",
+  };
+
+  const buttonStyles = {
+    width: "15vw", // Increased width
+    mb: 5,
+    backgroundColor: "#1976d2",
+    color: "white",
+    padding: "1rem 2rem", // Increased padding
+    fontSize: "1.25rem", // Increased font size
+    borderRadius: "0.25rem",
+    boxShadow: 2,
+    "&:hover": {
+      backgroundColor: "#115293",
+    },
+    textDecoration: "none",
+  };
+
+  const typographyStyles = {
+    mb: 30,
+  };
+
   return (
-    <div>
-      <p className="bg-red-600 ">home</p>
-    </div>
+    <Container sx={containerStyles}>
+      <Typography variant="h2" sx={typographyStyles}>
+        AtomicIQ
+      </Typography>
+      <Link to="/flashcard" style={{ textDecoration: "none" }}>
+        <Button sx={buttonStyles}>Flashcard</Button>
+      </Link>
+      <Link to="/wordle" style={{ textDecoration: "none" }}>
+        <Button sx={buttonStyles}>Wordle</Button>
+      </Link>
+      {/* Test purposes, have to remove this before submitting */}
+      <Link to="/quiz/0/results" style={{ textDecoration: "none" }}>
+        <Button sx={buttonStyles}>
+          Quiz result (remember to remove this button)
+        </Button>
+      </Link>
+    </Container>
   );
 }
